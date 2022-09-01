86f58a20a542d6693791f037c90cca5ad026a408
mhrpwfc88 <mh17pwfc@gmail.com>
2022-09-01 04:55:12 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    
+    extend: {
+      colors: {
+      'putih' : '#f8fafc',
+      'link' : ' #6d28d9 ',
+      'slatee' : ' #0f172a ',
+      'secondary' : '#64748b',
+      
+    },
+    fontFamily :{
+      'font1' : 'Kanit',
+      'font2' : 'Alumni Sans Pinstripe',
+      'font3' : 'Nunito',
+      'primary' : 'Varela Round',
+    },
+    screens: {
+        '2xl':'1320px',
+      },
+  },
+  },
+  plugins: [],
+}
