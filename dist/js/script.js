47d0ecb1bb6e6d11e67d3c158c78f4fde0c4fa6c
mhrpwfc88 <mh17pwfc@gmail.com>
2022-09-02 07:40:33 -0700

    dark mode

---
+++ dist/js/script.js
@@ -0,0 +1,15 @@
+let theme = 'light';
+
+function toggleDarkTheme(){
+    if(localStorage.getItem("theme") === 'dark'){
+        localStorage.setItem('theme', 'light');
+        document.documentElement.classList.remove('dark');
+    } else {
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.classList.add('dark');
+    }
+}
+document.querySelector('button').addEventListener('click', toggleDarkTheme);
+if(localStorage.getItem("theme") === 'dark'){
+    document.documentElement.classList.add('dark');
+}
