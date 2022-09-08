187e1cd77d162f31b8872c6685d9dbbd505b1690
mhrpwfc88 <mh17pwfc@gmail.com>
2022-09-07 21:12:40 -0700

    responseive iphone 12 pro

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,11 @@
 let theme = 'light';
 
+// document.querySelector('button').addEventListener('click', toggleDarkTheme);
+document.getElementById('darkMode').addEventListener('click', toggleDarkTheme);
+if(localStorage.getItem("theme") === 'dark'){
+    document.documentElement.classList.add('dark');
+};
+
 function toggleDarkTheme(){
     if(localStorage.getItem("theme") === 'dark'){
         localStorage.setItem('theme', 'light');
@@ -8,8 +14,4 @@ function toggleDarkTheme(){
         localStorage.setItem('theme', 'dark');
         document.documentElement.classList.add('dark');
     }
-}
-document.querySelector('button').addEventListener('click', toggleDarkTheme);
-if(localStorage.getItem("theme") === 'dark'){
-    document.documentElement.classList.add('dark');
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,27 +5,27 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      // padding: '16px',
     },
-    
+
     extend: {
       colors: {
-      'putih' : '#f8fafc',
-      'link' : ' #6d28d9 ',
-      'slatee' : ' #0f172a ',
-      'secondary' : '#64748b',
-      
-    },
-    fontFamily :{
-      'font1' : 'Kanit',
-      'font2' : 'Alumni Sans Pinstripe',
-      'font3' : 'Nunito',
-      'primary' : 'Varela Round',
-    },
-    screens: {
-        '2xl':'1320px',
+        'putih': '#f8fafc',
+        'link': ' #6d28d9 ',
+        'slatee': ' #0f172a ',
+        'secondary': '#64748b',
+
       },
-  },
+      fontFamily: {
+        'font1': 'Kanit',
+        'font2': 'Alumni Sans Pinstripe',
+        'font3': 'Nunito',
+        'primary': 'Varela Round',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
   },
   plugins: [],
 }
